5a1493ed3de6cd2901e9c89d2b5d88409d93a34a
camp140 <61010838@kmitl.ac.th>
2019-10-30 16:04:56 +0700

    Add Sum

--- Recursion.py
+++ Recursion.py
@@ -18,23 +18,21 @@ def sum1ToN(n):
 
 def printNto1(n):
     if n == 1:
-        print(n)
+        return 1
     else:
-        print(n)
-        printNto1(n - 1)
+        return n + printNto1(n - 1)
 
 
-# printNto1(7)
+# print(printNto1(7))
 
 def print1toN(n):
     if n == 1:
-        print(n)
+        return 1
     else:
-        print1toN(n - 1)
-        print(n)
+        return print1toN(n - 1) + n
 
 
-# print1toN(7)
+# print(print1toN(7))
 
 def fib(n):
     if n <= 1:
@@ -48,24 +46,64 @@ def fib(n):
 def binarySearch(lo, hi, x, l):
     if hi < lo:
         return None
-    mid = (lo+hi)//2
+    mid = (lo + hi) // 2
     if x == l[mid]:
         return mid
     elif l[mid] < x:
-        return binarySearch(mid+1, hi, x, l)
+        return binarySearch(mid + 1, hi, x, l)
     else:
-        return binarySearch(lo, mid-1, x, l)
+        return binarySearch(lo, mid - 1, x, l)
 
-# l = [1,2,3,4,5,6,7,8,9,10]
+
+# l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 # print(binarySearch(0,9,5,l))
 
 def move(n, from_rod, to_rod, aux_rod):
     if n == 1:
-        print('move disk',n, 'from', from_rod, 'to', to_rod)
+        print('move disk', n, 'from', from_rod, 'to', to_rod)
     else:
-        move(n-1, from_rod, aux_rod, to_rod)
-        print('move disk',n, 'from', from_rod, 'to', to_rod)
-        move(n-1, aux_rod, to_rod, from_rod)
+        move(n - 1, from_rod, aux_rod, to_rod)
+        print('move disk', n, 'from', from_rod, 'to', to_rod)
+        move(n - 1, aux_rod, to_rod, from_rod)
+
 
 # move(3,'A','C','B')
 
+def sum1(n, l):
+    if n is 0:
+        return 0
+    elif n is 1:
+        return l[0]
+    else:
+        return sum1(n - 1, l) + l[n - 1]
+
+
+# l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(sum1(len(l), l))
+
+
+def sum2(l, fromI, toI):
+    if fromI > toI:
+        return 0
+    elif fromI == toI:
+        return l[toI]
+    else:
+        return l[fromI] + sum2(l, fromI + 1, toI)
+
+
+# l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(sum2(l, 0, len(l) - 1))
+
+def sum3(l):
+    n = len(l)
+
+    if n is 0:
+        return 0
+    elif n is 1:
+        return l[0]
+    else:
+        return l[0] + sum3(l[1:])
+
+
+# l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(sum3(l))
